323057e75f718025a4224383cd1ed0be826f3825
Cristiano de Brito Godinho <cristiano.brito.g@gmail.com>
2024-02-01 20:58:18 -0300

    add
    curso

--- PycharmProjects/minicursohashtag/teste.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import pandas as pd
-import time
-time.sleep(5)
-pyautogui.press('win')
-pyautogui.write('chrome')
--- PycharmProjects/scripts_python/texte02.py
+++
@@ -1,4 +0,0 @@
-#Desafio um:Crie um script python que  leia o nome de uma pessoa e mostre uma mensagem
-#e mostre uma mensagem de boas vindas:
-nome=input('Qual o seu nome?:')
-print('prazer em te conhecer ,{}!'.format(nome))
